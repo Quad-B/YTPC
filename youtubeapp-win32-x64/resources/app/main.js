3854640ff3bdfdeac8d3ed957eb79f8ef8cc76d7
Phongsakorn Wisetthon <boyphongsakorn@outlook.com>
2017-06-11 15:55:49 +0700

    add,edit and delete

--- youtubeapp-win32-x64/resources/app/main.js
+++ youtubeapp-win32-x64/resources/app/main.js
@@ -1,23 +1,27 @@
 const {app, BrowserWindow} = require('electron');
-const path = require('path');
 const url = require('url');
+require('electron-context-menu')({
+    prepend: (params, browserWindow) => [{
+        label: 'Rainbow',
+        // Only show it when right-clicking images 
+        visible: params.mediaType === 'image'
+    }]
+});
 
 // init win
 let win;
 
 function createWindow() {
 	// Create browser window
-	win = new BrowserWindow({width:800, height:600, icon:__dirname+'img/youtubepcappicon.png'})
+	win = new BrowserWindow({width:800, height:600})
 
 	// Load index.html
-	win.loadURL(url.format({
-		pathname: path.join(__dirname, 'index.html'),
-		protocol: 'file:',
-		slashes: true
-	}));
+	win.loadURL('https://youtube.com');
 
-	// Open devtools
-	win.webContents.openDevTools();
+	win.webContents.on('new-window', function(e, url) {
+  	e.preventDefault();
+  	require('electron').shell.openExternal(url);
+	});
 
 	win.on('closed', () => {
 		win = null;
@@ -29,7 +33,7 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed
 app.on('window-all-closed', () => {
-	if(process.platform !== 'darwin'){
+	if(process.platform !== 'win32'){
 		app.quit();
 	}
 });
