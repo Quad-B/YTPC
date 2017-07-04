1132b95e71cb45b3ec0a1bba2be87d104eea4c5a
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2017-07-05 05:51:02 +0700

    bug ?

--- main.js
+++ main.js
@@ -13,20 +13,20 @@ require('electron-context-menu')({
 let win;
 
 function createWindow() {
-	// Create browser window
-	win = new BrowserWindow({width:800, height:600, icon:__dirname+'/img/youtubepcappicon.png'})
+  // Create browser window
+  win = new BrowserWindow({width:800, height:600, icon:__dirname+'/img/youtubepcappicon.png'})
 
-	// Load index.html
-	win.loadURL('http://brsocial.esy.es/ytpc');
+  // Load index.html
+  win.loadURL('http://brsocial.esy.es/ytpc');
 
-	win.webContents.on('new-window', function(e, url) {
-  	e.preventDefault();
-  	require('electron').shell.openExternal(url);
-	});
+  win.webContents.on('new-window', function(e, url) {
+    e.preventDefault();
+    require('electron').shell.openExternal(url);
+  });
 
-	win.on('closed', () => {
-		win = null;
-	});
+  win.on('closed', () => {
+    win = null;
+  });
 }
 
 // Run create window function
@@ -34,7 +34,7 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed
 app.on('window-all-closed', () => {
-	if(process.platform !== 'win32'){
-		app.quit();
-	}
+  if(process.platform !== 'win32'){
+    app.quit();
+  }
 });
